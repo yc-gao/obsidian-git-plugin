befc0bf8188cc0e3375418510dd7c1e5234a4ca3
ycgao <yc_x@outlook.com>
2026-02-27 06:42:15 +0000

    update

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,22 +1,24 @@
 import esbuild from "esbuild";
+
 import process from "process";
 import { builtinModules } from 'node:module';
 
 const prod = (process.argv[2] === "production");
 const context = await esbuild.context({
-    platform: "node",
-    entryPoints: ["src/main.ts"],
     bundle: true,
-    external: [
-        "obsidian",
-        ...builtinModules],
+    platform: "node",
     format: "cjs",
     target: "es2018",
     logLevel: "info",
     sourcemap: prod ? false : "inline",
     treeShaking: true,
-    outfile: "main.js",
     minify: prod,
+
+    entryPoints: ["src/main.ts"],
+    outfile: "dist/main.js",
+    external: [
+        "obsidian",
+        ...builtinModules],
 });
 
 if (prod) {
